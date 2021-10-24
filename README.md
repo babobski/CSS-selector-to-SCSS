d8d83177ca09195ede9855a289f93872af998a28
Babobski <info@babobski.nl>
2021-10-24 13:59:04 +0200

    added convert to rule logic and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # CSS selector to SCSS
 
+This extensions allows you to convert CSS selectors or rules to SCSS.
+It works with the clipboard values you would get form:
+
+![Copy rule or selector](images/copy-selector-or-rule-in-chrome.jpg)
+
+This extension got two commands:
+* Convert CSS selector to SCSS from clipboard
+* Convert CSS rule to SCSS from clipboard
+
+## Convert CSS selector to SCSS from clipboard
+This command wil turn a selector in a nested SCSS structure.  
+Executing the command with the following value on your clipboard:
+
+```css
+#sidebar .title
+```
+
+Will result in the following result:
+```scss
+#sidebar {
+    .title {
+        |
+    }
+}
+```
+
+## Convert CSS rule to SCSS from clipboard 
+This command turns a copied css rule to a nested SCSS structure.  
+The following copied code:
+
+```css
+#sidebar .title {
+	flex: 3;
+	font-size: 17px;
+	align-self: center;
+	white-space: nowrap;
+	opacity: 0;
+	transition: opacity 300ms ease;
+}
+```
+
+Will result in:
+```scss
+#sidebar {
+    .title {
+        flex: 3;
+        font-size: 17px;
+        align-self: center;
+        white-space: nowrap;
+        opacity: 0;
+        transition: opacity 300ms ease;
+    }
+}
+```
--- src/extension.ts
+++ src/extension.ts
@@ -1,155 +1,199 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('css-selector-to-scss.ConvertFromClipboard', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	let disposable = vscode.commands.registerCommand('css-selector-to-scss.ConvertSelectorFromClipboard', () => {
 		const editor = vscode.window.activeTextEditor;
 
 		vscode.env.clipboard.readText().then((clipText) => {
 			if (clipText.length > 0) {
-				if (clipText.indexOf(',') !== -1) {
-					let lines = clipText.split(',');
-
-					clipText = lines[0];
-					vscode.window.showErrorMessage('CSS selector to SCSS only works with single rules. Only processed the first line.');
-				}
-				let cliptextArr = clipText.split(/[\s:]+/gm);
-				let snippetStr = '';
-				let level = 0;
-				let snippetPrefix = (level: number) => {
-					let output = '';
-
-					for (let e = 0; e < level; e++) {
-						output = output + '\t';
-						
-					}
-
-					return output;
-				};
-
-				let PseudoPrefix = (selector: string) => {
-					const PseudoList = [
-						':after',
-						':before',
-						'active',
-						'checked',
-						'disabled',
-						'empty',
-						'enabled',
-						'first-child',
-						'first-of-type',
-						'focus',
-						'hover',
-						'in-range',
-						'invalid',
-						'last-child',
-						'last-of-type',
-						'link',
-						'only-of-type',
-						'only-child',
-						'optional',
-						'out-of-range',
-						'read-only',
-						'read-write',
-						'required',
-						'root',
-						'target',
-						'valid',
-						'visited'
-					];
-
-					const PseudoListDouble = [
-						'after',
-						'before',
-						'first-letter',
-						'first-line',
-						'marker',
-						'selection'
-					];
-
-					const PseudoStartsWithList = [
-						'lang(',
-						'not(',
-						'nth-child(',
-						'nth-last-child(',
-						'nth-last-of-type(',
-						'nth-of-type(',
-					];
-
-					if (PseudoList.indexOf(selector) !== -1) {
-						return '&:';
-					}
-
-					if (PseudoListDouble.indexOf(selector) !== -1) {
-						return '&::';
-					}
-
-					for (let i = 0; i < PseudoStartsWithList.length; i++) {
-						const element = PseudoStartsWithList[i];
-						if (selector.indexOf(element) === 0) {
-							return '&:';
-						}
-					}
-
-					return '';
-				};
-
-				const validOperators = [
-					'+',
-					'>',
-					'~'
-				];
-
-				
-				for (let i = 0; i < cliptextArr.length; i++) {
-					const clipEl = cliptextArr[i];
-
-					if (level === 0) {
-						snippetStr = PseudoPrefix(clipEl) + clipEl + ' {\n';
-					} else {
-						if (clipEl.length === 1 && validOperators.indexOf(clipEl) !== -1) {
-							i++;
-							snippetStr = snippetStr + snippetPrefix(level) + PseudoPrefix(clipEl) + clipEl + ' ' + PseudoPrefix(cliptextArr[i]) + cliptextArr[i] + ' {\n';
-							level++;
-							continue;
-						}
-						snippetStr = snippetStr + snippetPrefix(level) + PseudoPrefix(clipEl) + clipEl + ' {\n';
-					}
-					level++;
-				}
-
-				for (let w = 0; w < level; w++) {
-					snippetStr = snippetStr + '\t';
-				}
-
-				snippetStr = snippetStr + '$0\n';
-
-				let revPrev = level;
-
-				for (let k = 0; k < level; k++) {
-					revPrev--;
-					snippetStr = snippetStr + snippetPrefix(revPrev) + '}' + (revPrev > 0 ? '\n' : '');
-				}
-				editor?.insertSnippet(new vscode.SnippetString(snippetStr));
+				convertSelectorToSnippet(clipText, () => {});
 			}
 		});
 	});
 
 	context.subscriptions.push(disposable);
+
+	let disposable2 = vscode.commands.registerCommand('css-selector-to-scss.ConvertRuleFromClipboard', async () => {
+		const editor = vscode.window.activeTextEditor;
+
+		vscode.env.clipboard.readText().then(async (clipText) => {
+			if (clipText.length > 0) {
+				let matchResults = [...clipText.matchAll(/^([^{]+){([^}]+)}/gm)];
+				if (matchResults.length > 0 && matchResults[0].length === 3) {
+					
+					let selector = matchResults[0][1].toString().replace(/\s$/, '');
+					let values = matchResults[0][2].toString().replace(/(\r|\n|\t|^\s+)/gm, '');
+
+					vscode.env.clipboard.writeText(selector).then(async text => {
+						convertSelectorToSnippet(selector, () => {
+							let valuesArr = values.replace(/;$/g, '').split(';');
+							let snippetString = '';
+	
+							for (let i = 0; i < valuesArr.length; i++) {
+								const val = valuesArr[i];
+								snippetString = snippetString + val + ';' + ((valuesArr.length-1) === i ? '' : '\n');
+							}
+							editor?.insertSnippet(new vscode.SnippetString(snippetString));
+						});
+					});
+				}
+			
+			}
+		});
+	});
+
+	context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+let convertSelectorToSnippet = (clipText: string, callback: (() => void)) => {
+	const editor = vscode.window.activeTextEditor;
+	// If is multiple lines, only process the first line.
+	if (clipText.indexOf(',') !== -1) {
+		let lines = clipText.split(',');
+		clipText = lines[0];
+		vscode.window.showErrorMessage('CSS selector to SCSS only works with single rules. Only processed the first line.');
+	}
+	// Split on space and : returning all selectors and pseudo elements 
+	let cliptextArr = clipText.replace(/\:\:/g, ':').split(/[\s:]+/gm),
+		snippetStr = '',
+		level = 0,
+		// Calculates the leading indent
+		snippetPrefix = (level: number) => {
+			let output = '';
+
+			for (let e = 0; e < level; e++) {
+				output = output + '\t';	
+			}
+			return output;
+		},
+		// check if is a Pseudo element
+		PseudoPrefix = (selector: string) => {
+			const PseudoList = [
+				'active',
+				'checked',
+				'disabled',
+				'empty',
+				'enabled',
+				'first-child',
+				'first-of-type',
+				'focus',
+				'hover',
+				'in-range',
+				'invalid',
+				'last-child',
+				'last-of-type',
+				'link',
+				'only-of-type',
+				'only-child',
+				'optional',
+				'out-of-range',
+				'read-only',
+				'read-write',
+				'required',
+				'root',
+				'target',
+				'valid',
+				'visited'
+			];
+
+			const PseudoListDouble = [
+				'after',
+				'before',
+				'first-letter',
+				'first-line',
+				'marker',
+				'selection'
+			];
+
+			const PseudoStartsWithList = [
+				'lang(',
+				'not(',
+				'nth-child(',
+				'nth-last-child(',
+				'nth-last-of-type(',
+				'nth-of-type(',
+			];
+
+			// Check if is pseudo element
+			if (PseudoList.indexOf(selector) !== -1) {
+				return '&:';
+			}
+
+			if (PseudoListDouble.indexOf(selector) !== -1) {
+				return '&::';
+			}
+
+			for (let i = 0; i < PseudoStartsWithList.length; i++) {
+				const element = PseudoStartsWithList[i];
+				if (selector.indexOf(element) === 0) {
+					return '&:';
+				}
+			}
+
+			return '';
+		};
+
+	const validOperators = [
+		'+',
+		'>',
+		'~'
+	];
+
+	
+	for (let i = 0; i < cliptextArr.length; i++) {
+		const clipEl = cliptextArr[i];
+
+		// If it is a valid operator prepend it to the next result
+		if (clipEl.length === 1 && validOperators.indexOf(clipEl) !== -1) {
+			i++;
+			snippetStr = snippetStr + snippetPrefix(level) + PseudoPrefix(clipEl) + clipEl + ' ' + PseudoPrefix(cliptextArr[i]) + cliptextArr[i] + ' {\n';
+			level++;
+			continue;
+		}
+
+		// If it contains multiple classes or ids
+		const regex = /[#\.][a-z-_]+[#.][a-z-_]+/gi;
+		if (regex.test(clipEl)) {
+			let transFromItem = clipEl;
+			transFromItem = transFromItem.replace(/\#/g, '|#').replace(/\./g, '|.');
+			let loopArray = transFromItem.split('|');
+			let first = true;
+			if (loopArray[0] === '') {
+				loopArray.shift();
+			}
+			
+			for (let o = 0; o < loopArray.length; o++) {
+				const element = loopArray[o];
+				snippetStr = snippetStr + snippetPrefix(level) + (!first ? '&' : '') + element + ' {\n';
+				first = false;
+				level++;
+			}
+			continue;
+		}
+		// Build snippet
+		snippetStr = snippetStr + snippetPrefix(level) + PseudoPrefix(clipEl) + clipEl + ' {\n';
+	
+		level++;
+	}
+
+	// Build leading indend for value
+	snippetStr = snippetStr + snippetPrefix(level);
+
+	// Add value/final cursor position
+	snippetStr = snippetStr + '$0\n';
+
+	let revPrev = level;
+	// Build backwards closing tags
+	for (let k = 0; k < level; k++) {
+		revPrev--;
+		snippetStr = snippetStr + snippetPrefix(revPrev) + '}' + (revPrev > 0 ? '\n' : '');
+	}
+	// Insert the snippet
+	editor?.insertSnippet(new vscode.SnippetString(snippetStr)).then(insterted => {
+		callback();
+	});
+};
